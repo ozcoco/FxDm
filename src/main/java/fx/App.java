87a2ea41e510c3ee058b5bc3d79216f1d4e63635
ozcomcn <857527916@qq.com>
2019-01-25 01:36:33 +0800

    init

---
+++ src/main/java/fx/App.java
@@ -0,0 +1,28 @@
+package fx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ */
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/layout/main.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+    }
+}
---
+++ src/main/java/fx/Main.java
@@ -0,0 +1,15 @@
+package fx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Main {
+
+    @FXML
+    public void onStart(ActionEvent event) {
+
+        System.out.println("!!!!!!!!!!!!!!!!!!!");
+
+    }
+
+}
